d781102bfe5fbcf2d66293074c66647aacdf16a6
gary <gary.white@lodgit.com>
2022-05-30 12:59:49 +0200

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+function noBoringZeros(n) {
+    // your code
+}
+
+console.log(noBoringZeros(1450)); // 145
+console.log(noBoringZeros(noBoringZeros(960000))); // 96
+console.log(noBoringZeros(1050)); // 105
+console.log(noBoringZeros(-1050)); // -105
